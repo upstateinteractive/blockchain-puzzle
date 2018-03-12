fba5d2755c7e20f62787ae7cf4be7ffa2a8de00f
Ubuntu <ubuntu@ip-172-31-83-254.ec2.internal>
2018-03-12 18:26:41 +0000

    working on Puzzles contract

--- contracts/Puzzles.sol
+++ contracts/Puzzles.sol
@@ -1,9 +1,19 @@
 pragma solidity ^0.4.17;
 
+import "./UserFactory.sol";
+
 contract Puzzles { 
-    
-//    function levelOne() returns (bool success) {
-//	
-//    }
+
+//    mapping(address => uint) public userAnswer;   
+
+//    UserFactory user = UserFactory(UserFactoryAddress);
+
+    function levelOne(uint answer) public returns (bool success) {
+//	userAnswer[msg.sender] = answer;	
+
+	if(answer == 1)
+	    incrementLevel();
+	    return true;	
+    }
 
 }
--- contracts/UserFactory.sol
+++ contracts/UserFactory.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.17;
 
+import "./Puzzles.sol";
+
 contract UserFactory {  
 
     struct User{
