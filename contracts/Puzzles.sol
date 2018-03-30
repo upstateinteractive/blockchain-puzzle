11997784bb5bf087d0c1c9a809993d91a6e8e508
Doug Crescenzi <doug@telapi.com>
2018-03-30 12:03:12 -0400

    now Puzzles return booleans for the front-end

--- contracts/Puzzles.sol
+++ contracts/Puzzles.sol
@@ -4,53 +4,69 @@ import "./UserFactory.sol";
 
 contract Puzzles is UserFactory { 
 
-      // amount a user has to pay to make a guess
-      uint payToPlayFee = .01 ether;
-
-//    mapping(address => uint) public userAnswer;   
-
-//    UserFactory user = UserFactory(UserFactoryAddress);
+// 	amount a user has to pay to make a guess
+    uint payToPlayFee = .01 ether;
 
     function payWinner() internal {
+
         uint balance = this.balance;
+
         owner.transfer(balance / 2);
+
         msg.sender.transfer(balance / 2);
     }
 
-    function levelOne(uint answer) public payable {
-		//	userAnswer[msg.sender] = answer;
+    function levelOne(uint answer) public payable returns (bool) {
 
 			uint currentLevel = findLevel(msg.sender); 
 
 			require(msg.value == payToPlayFee);
 
 			if (currentLevel == 1 && answer == 1) {
+
 				incrementLevel();
+
+				return true;
 			}
+			else {
+
+            	return false;
+        	}
     }
 
-	function levelTwo(uint answer) public payable {
-		//	userAnswer[msg.sender] = answer;	
+	function levelTwo(uint answer) public payable returns (bool) {
 			
 			uint currentLevel = findLevel(msg.sender);		
 
 			require(msg.value == payToPlayFee);
 
 			if (currentLevel ==2 && answer == 2) {
+
 				incrementLevel();
+
+				return true;
 			}
+			else {
+
+            	return false;
+        	}
 		}
 
-	function finalLevel(uint answer) public payable {
-	//	userAnswer[msg.sender] = answer;
+	function levelSix(uint answer) public payable returns (bool) {
 
 		uint currentLevel = findLevel(msg.sender);
 	
 		require(msg.value == payToPlayFee);
 
-		if (currentLevel == 10 && answer == 5) {
+		if (currentLevel == 6 && answer == 6) {
+
 			payWinner();
+			
 			incrementLevel();
 		}
+		else {
+
+            return false;
+        }
     }
 }
